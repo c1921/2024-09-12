7ff5f48684d6c21a8cb538ded87261feb38e3e94
C1921 <w915504817@gmail.com>
2024-09-13 10:53:17 +0800

    移除了 "View Details" 按钮

--- src/components/FamilyList.vue
+++ src/components/FamilyList.vue
@@ -3,24 +3,20 @@
     <div class="buttons mb-4">
       <button class="button is-primary" @click="$emit('addCharacter')">Add Random Character</button>
     </div>
-    <table class="table is-fullwidth">
+    <table class="table is-fullwidth is-hoverable">
       <thead>
         <tr>
           <th>Family Name</th>
           <th>Members</th>
-          <th>Actions</th>
         </tr>
       </thead>
       <tbody>
         <tr v-for="family in families" :key="family.id" 
-            :class="{ 'is-selected': selectedFamilyId === family.id }">
+            :class="{ 'is-selected': selectedFamilyId === family.id }"
+            @click="$emit('selectFamily', family)"
+            style="cursor: pointer;">
           <td>{{ family.name }} (ID: {{ family.id.slice(0, 4) }})</td>
           <td>{{ family.members.length }}</td>
-          <td>
-            <button class="button is-small is-info" @click="$emit('selectFamily', family)">
-              View Details
-            </button>
-          </td>
         </tr>
       </tbody>
     </table>
